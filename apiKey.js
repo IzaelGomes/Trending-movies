fbceb3d115d658d696b31c579bddd4bc78687649
izael gomes <izaelgomes127@gmail.com>
2022-07-14 09:42:34 -0300

    final project

---
+++ apiKey.js
@@ -0,0 +1,23 @@
+export var key = 'cb35b229f134f593da02fa9ded6da8ca'
+
+ /* if(favorite.classList.contains('heart')){
+              favorite.classList.remove('heart')
+               favorite.classList.add('full-heart')
+               favorite.src = 'assets/full-heart.svg'
+
+               favMovies.push(movie)
+
+               movieJson = JSON.stringify(favMovies)
+               localStorage.setItem('favoriteMovies', movieJson)
+               
+
+            }else{
+              favorite.classList.remove('full-heart')
+              favorite.classList.add('heart')
+              favorite.src = 'assets/Heart.svg'
+
+              favMovies.splice(  ,1) 
+              movieJson = JSON.stringify(favMovies)
+              localStorage.setItem('favoriteMovies', movieJson)   
+               
+            } */
--- script.js
+++ script.js
@@ -1,111 +1,145 @@
 
-/*
-movielist = [  
-    {title:'Avengers Endgame', img:'vingadores.svg', year:2021, rate:'9.3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  Duis aute irure dolor in reprehenderit in voluptate veliDuis aute irure dolor in reprehenderit in voluptate veli Duis aute irure dolor in reprehenderit in voluptate veli'}, 
-    {title: 'Avatar- a lenda de aang', img:'vingadores.svg', year:2016, rate:'10.0', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  Duis aute irure dolor in reprehenderit in voluptate veliDuis aute irure dolor in reprehenderit in voluptate veli Duis aute irure dolor in reprehenderit in voluptate veli' }
+import {key} from './apiKey.js'
 
-]*/
+const filmeM = document.querySelector('.movie-name')
+const container = document.querySelector('.container')
+const main = document.querySelector('main')
+const showFavMovie = document.querySelector('.fav-movies')
+const containerFilme = document.createElement('div')
+let movieJson
 
+ 
 
-/*async function getpopularMovies(){
-    fetch('https://api.themoviedb.org/3/movie/76341?api_key=cb35b229f134f593da02fa9ded6da8ca')
-    .then(async res => {
+//Chamada da API
+async function getpopularMovies(){
 
-        if(!res.ok){
-            throw new Error(res.status)
-        }
+  let req = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${key}`)
+  let filme = await req.json()
+  let  movies =  filme.results
+       console.log(movies)
+       movies.forEach(movieListShow =>{
+           createElementsMovies(movieListShow)
+           
+       })
+      
+  }
 
-        const movie = await res.json()
+//função para busca dos filmes
+async function searchMovie(movieName){
 
-        console.log(movie)
+    let req = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${movieName}`)
+    let callMovie = await req.json()
+    let catchMovie = callMovie.results
 
-        movie.map((item)=>{
-
-            const movieList = document.createElement('div')
-            const  movieInformation = document.createElement('div')
-            const  container = document.querySelector('.container')
-            const  img = document.createElement('img')
-            const  movieTitle= document.createElement('div')
-            const  movieName = document.createElement('h2')
-            const  icones = document.createElement('div')
-            const rate = document.createElement('img')
-            const favorite = document.createElement('img')
-            const  spanRate = document.createElement('span')
-            const spanFavorite = document.createElement('span')
-            const description = document.createElement('div')
-            
-            
-            movieList.classList.add('movies-list')
-            movieInformation.classList.add('movie-information')
-            img.classList.add('movie-img')
-            movieTitle.classList.add('movie-title')
-            icones.classList.add('icones')
-            rate.classList.add('star')
-            favorite.classList.add('heart')
-            description.classList.add('movie-description')
-            
-             rate.setAttribute('src', 'Star.svg')
-            favorite.setAttribute('src', 'Heart.svg')
-            img.setAttribute('src', item.backdrop_path)
-            
-            movieName.innerHTML = `${item.original_title} (${item.release_date})`
-            description.textContent = item.overview 
-            
-            
-            container.appendChild(movieList)
-            movieList.appendChild(movieInformation)
-            movieList.appendChild(description)
-            movieInformation.appendChild(img)
-            movieInformation.appendChild(movieTitle)
-            movieTitle.appendChild(movieName)
-            movieTitle.appendChild(icones)
-            icones.appendChild(spanRate)
-            spanRate.textContent = `${item.popularity}`
-            spanRate.appendChild(rate)
-            icones.appendChild(spanFavorite)
-            spanFavorite.textContent = 'Favoritar' 
-            spanFavorite.appendChild(favorite)
-            
-            })
-        
-
-    })
-    
-    .catch(error => console.log(error))
+      catchMovie.forEach(movieCall => {
+      createElementsMovies(movieCall)
+    });
 }
 
-*/
+// função que vai realizar o evento de clique para pesquisa dos filmes
+function movieSearch(){
+  filmeM.addEventListener('keyup', function(event){
+    //Quando a tecla enter for clicada é limpado o container dos filmes e a função de 
+    //procura o nome do filme é chamada
+     if(event.keyCode == 13){
+         containerFilme.innerHTML = ' ' 
+         searchMovie(filmeM.value)
+ }
+})
+}
 
-function getpopularMovies(){
-    fetch('https://api.themoviedb.org/3/trending/all/day?api_key=cb35b229f134f593da02fa9ded6da8ca')
-   .then(  res => {
-    return res.json()
-   })
-   .then(filme =>{
+//Evento que irá mostrar os filmes favoritaso pelo usuário
+ showFavMovie.addEventListener('click', ()=>{
+      if(showFavMovie.checked ){
+        containerFilme.innerHTML = ''
+        movieJson =  localStorage.getItem('favoriteMovies')
+       const dataMoviesShow = JSON.parse(movieJson)
+        dataMoviesShow.forEach(movieData =>{
+
+          createElementsMovies(movieData)
+        })
+ 
+  }else{
+    containerFilme.innerHTML = ''
+    getpopularMovies()
+  }
+
+})
+
+function getFavoriteMovies() {
+  return JSON.parse(localStorage.getItem('favoriteMovies'))
+}
+
+
+//função que irá salvar filmes
+function saveToLocalStorage(movie){
+  const movies =  getFavoriteMovies() || []
+  movies.push(movie) // transforma o json em texto para ser salvo no localStore
+   movieJson = JSON.stringify(movies)
+  localStorage.setItem('favoriteMovies', movieJson )
+
+}
+
+function removeMovieFromLocalStorage(id){
+  //a variavel moviesSaved recebe a lista que filmes que está salva no localStorage.
+  const moviesSaved = getFavoriteMovies() || []
+
+  //moviesFind verifica se tem algum filme que está no banco com o mesmo id dos filmes que estão sendo 
+  //listados na tela principal , e então vai retornar para a variável.
+  const movieFind = moviesSaved.find(movieSaved =>  movieSaved.id == id)
+  
+  //newList irá filtrar todos os filmes do localStore que tem o id diferente do movieFind e vai armazenar
+  //essa nova lista na variável.
+ const newList = moviesSaved.filter(movieSave => movieSave.id !== movieFind.id)
+ localStorage.setItem('favoriteMovies', JSON.stringify(newList))
+}
+
+
+
+ //função que verifica o clique na imagem para favoritar o filme
+ function favoriteButtonPressed(event, movie){
+  const favoriteState = {
+    favorited: 'assets/full-heart.svg',
+    notFavorited: 'assets/Heart.svg'
+  }
+  //verifica se a imagem esta como notfavorited e adiciona a outra imagem
+  if(event.target.src.includes(favoriteState.notFavorited)){
+    event.target.src = favoriteState.favorited
+    saveToLocalStorage(movie) // executa a função de salvar
+  } else{
+    event.target.src = favoriteState.notFavorited
+    removeMovieFromLocalStorage(movie.id) //executa a função de remover 
+  }
+ }
+
+ //checa se o filme que está no localStore é o mesmo que está na tela de início
+ //se estiver ele retorna true, que significa que o filme está favoritado
+   function checkMovieIsFavorite(id){
+    const movies = getFavoriteMovies() || []
+    return movies.find(movie => movie.id == id)
     
-      movie =   filme.results
+  }
 
-      console.log(movie)
+       function createElementsMovies(movie){
 
-  for(let i in movie){
+        // passa o id do filme e executa a função no qual retorna 
+        // true ou false 
+        const isfavorite = checkMovieIsFavorite(movie.id) 
 
-    const imagem = `https://image.tmdb.org/t/p/w500${movie[i].backdrop_path}`
-
-
+        const imagem = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
         const movieList = document.createElement('div')
         const  movieInformation = document.createElement('div')
-        const  container = document.querySelector('.container')
         const  img = document.createElement('img')
         const  movieTitle= document.createElement('div')
         const  movieName = document.createElement('h2')
         const  icones = document.createElement('div')
-        const rate = document.createElement('img')
-        const favorite = document.createElement('img')
+        const  rate = document.createElement('img')
+        const  favorite = document.createElement('img')
         const  spanRate = document.createElement('span')
         const spanFavorite = document.createElement('span')
         const description = document.createElement('div')
         
-        
+        containerFilme.classList.add('container-filmes')
         movieList.classList.add('movies-list')
         movieInformation.classList.add('movie-information')
         img.classList.add('movie-img')
@@ -113,17 +147,24 @@ function getpopularMovies(){
         icones.classList.add('icones')
         rate.classList.add('star')
         favorite.classList.add('heart')
+        // Dependendo do retorno a imagem será trocada
+        favorite.src = isfavorite ? 'assets/full-heart.svg' : 'assets/Heart.svg' 
+
+        //É passado um evento de click para a imagem, onde será realizado a troca de imagem 
+        favorite.addEventListener('click', (event)=> favoriteButtonPressed(event, movie))
+          
+
+        spanFavorite.classList.add('span-favorite')
         description.classList.add('movie-description')
         
-         rate.setAttribute('src', 'assets/Star.svg')
-        favorite.setAttribute('src', 'assets/Heart.svg')
+        rate.setAttribute('src', 'assets/Star.svg')
         img.src =  imagem
+
+        movieName.textContent = `${movie.title} (${movie.release_date})`
+        description.textContent = movie.overview 
         
-        movieName.textContent = `${movie[i].title} (${movie[i].release_date})`
-        description.textContent = movie[i].overview 
-        
-        
-        container.appendChild(movieList)
+        main.appendChild(containerFilme)
+        containerFilme.appendChild(movieList)
         movieList.appendChild(movieInformation)
         movieList.appendChild(description)
         movieInformation.appendChild(img)
@@ -131,21 +172,15 @@ function getpopularMovies(){
         movieTitle.appendChild(movieName)
         movieTitle.appendChild(icones)
         icones.appendChild(spanRate)
-        spanRate.textContent = `${movie[i].vote_average}`
+        spanRate.textContent = `${movie.vote_average}`
         spanRate.appendChild(rate)
         icones.appendChild(spanFavorite)
         spanFavorite.textContent = 'Favoritar' 
         spanFavorite.appendChild(favorite)
-  }
-
-        })
-    
-  
-
-   .catch(error => console.log(error))
-}
+      }
 
 getpopularMovies()
+movieSearch()
 
 
 
